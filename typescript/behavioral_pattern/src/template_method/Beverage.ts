270ab1a30dc2a1cf0da27951a1e470958d041338
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-10-17 00:43:18 +0530

    Implement the beverage abstract class

---
+++ typescript/behavioral_pattern/src/template_method/Beverage.ts
@@ -0,0 +1,30 @@
+abstract class Beverage {
+    public _prepareBeverage(): void {
+        this.boilWater();
+        this.brew();
+        this.pourInCup();
+        if (this.customerWantsCondiments()) {
+            this.addCondiments();
+        }
+        this.serve();
+    }
+
+    private boilWater(): void {
+        console.log("Boiling water...");
+    }
+
+    private pourInCup(): void {
+        console.log("Pouring into cup...");
+    }
+
+    private serve(): void {
+        console.log("Your beverage is ready!");
+    }
+
+    protected abstract brew(): void;
+    protected abstract addCondiments(): void;
+
+    protected customerWantsCondiments(): boolean {
+        return true;
+    }
+}
