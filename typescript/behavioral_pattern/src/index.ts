1c83ac9ea50f1ac18d1bf0d0c9b4d9d5a6f1680e
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-10-17 00:53:17 +0530

    add one example of tempalate method pattern using ts

--- typescript/behavioral_pattern/src/index.ts
+++ typescript/behavioral_pattern/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Behavioral Design Pattern");
+import Tea from "./template_method/Tea";
+import Coffee from "./template_method/Coffee";
+import HotChocolate from "./template_method/HotChocolate";
+import Beverage from "./template_method/Beverage";
+
+// Template Method Example
+
+console.log("Example of Template Method using the Tea, Coffee, HotChocolate class.\n");
+console.log("=== Making Tea ===\n");
+const tea: Beverage = new Tea();
+tea._prepareBeverage();
+
+console.log("\n=== Making Coffee ===\n");
+const coffee: Beverage = new Coffee();
+coffee._prepareBeverage();
+
+console.log("\n=== Making Hot Chocolate ===\n");
+const hotChocolate: Beverage = new HotChocolate();
+hotChocolate._prepareBeverage();
+
