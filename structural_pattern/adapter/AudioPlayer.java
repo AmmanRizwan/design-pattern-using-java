3d6269fb496bcfd80905cc6a3412f5b7221c80be
AmmanRizwan <ammanrizwan00007@gmail.com>
2026-02-24 11:44:13 +0530

    Implementing the new Design pattern of structural pattern called adapter

---
+++ structural_pattern/adapter/AudioPlayer.java
@@ -0,0 +1,18 @@
+package adapter;
+
+public class AudioPlayer implements MediaPlayer {
+    
+    private MediaAdapter adapter;
+
+    @Override
+    public void play(String audioType, String fileName) {
+        if (audioType.equalsIgnoreCase("mp3")) {
+            System.out.println("Playing MP3 file: " + fileName);
+        } else if (audioType.equalsIgnoreCase("vlc") || audioType.equalsIgnoreCase("mp4")) {
+            adapter = new MediaAdapter(audioType);
+            adapter.play(audioType, fileName);
+        } else {
+            System.out.println("Invalid media type: " + audioType + " not supported.");
+        }
+    }
+}
