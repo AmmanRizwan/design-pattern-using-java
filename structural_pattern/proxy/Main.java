da2fe7d4c4b32624ee42fe799d35279e7654f64f
AmmanRizwan <ammanrizwan00007@gmail.com>
2026-03-25 12:17:41 +0530

    Implement an example of proxy design pattern

--- structural_pattern/proxy/Main.java
+++ structural_pattern/proxy/Main.java
@@ -2,6 +2,30 @@ package proxy;
 
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("=== Using Proxy Pattern ===");
+        System.out.println();
+
+        Image image1 = new ProxyImage("photo1.jpg");
+        Image image2 = new ProxyImage("photo2.jpg");
+
+        System.out.println("--- First display call");
+        image1.display();
+        System.out.println("\n--- Second display call (cached) ---");
+        image1.display();
+
+        System.out.println("\n--- Display different image ---");
+        image2.display();
+
+        System.out.println("\n=== Protection Proxy Example ===\n");
+        Document doc = new ProxyDocument("admin");
+        doc.displayContent();
+        doc.editContent("New Content");
+
+        System.out.println();
+
+        Document restritDocument = new ProxyDocument("guest");
+
+        restritDocument.displayContent();
+        restritDocument.editContent("Trying to edit");
     }
 }
