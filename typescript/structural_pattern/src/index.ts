66c2aa74e1a307c162b31666151722ba514c6def
AmmanRizwan <ammanrizwan00007@gmail.com>
2026-04-04 17:10:12 +0530

    give one example using the audio player adapter

--- typescript/structural_pattern/src/index.ts
+++ typescript/structural_pattern/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Structoral Pattern Project");
+import MediaPlayer from "./adapter/MediaPlayer";
+import AudioPlayer from "./adapter/AudioPlayer";
+
+// Adapter Example
+
+console.log("Adapter Example using the AudioPlayer Class:\n");
+
+const audioPlayer: MediaPlayer = new AudioPlayer();
+
+audioPlayer.play("mp3", "song.mp3");
+audioPlayer.play("mp4", "video.mp4");
+audioPlayer.play("vlc", "movie.vlc");
