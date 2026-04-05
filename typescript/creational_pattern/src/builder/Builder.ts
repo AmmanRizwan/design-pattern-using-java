f1093facc7fa8024144430f323cfc053a83942f3
AmmanRizwan <ammanrizwan00007@gmail.com>
2026-04-05 13:04:58 +0530

    Implement the builder design pattern

--- typescript/creational_pattern/src/builder/Builder.ts
+++ typescript/creational_pattern/src/builder/Builder.ts
@@ -1,7 +1,31 @@
 import House from "./House";
 
 class Builder {
+    public foundation: string = "";
+    public walls: string = "";
+    public roof: string = "";
 
+    constructor() {
+    }
+
+    public setFoundation(foundation: string): Builder {
+        this.foundation = foundation;
+        return this;
+    }
+
+    public setWalls(walls: string): Builder {
+        this.walls = walls;
+        return this;
+    }
+
+    public setRoof(roof: string): Builder {
+        this.roof = roof;
+        return this;
+    }
+
+    public build(): House {
+        return new House(this);
+    }
 }
 
 export default Builder;
--- typescript/creational_pattern/src/builder/House.ts
+++ typescript/creational_pattern/src/builder/House.ts
@@ -1,7 +1,19 @@
 import Builder from "./Builder";
 
 class House {
+    private _foundation: string;
+    private _walls: string;
+    private _roof: string;
 
+    public constructor(builder: Builder) {
+        this._foundation = builder.foundation;
+        this._walls = builder.walls;
+        this._roof = builder.roof;
+    }
+
+    public getCart(): void {
+        console.log(`Car: foundation:${this._foundation}, walls:${this._walls}, roof:${this._roof}`);
+    }
 }
 
 export default House;
