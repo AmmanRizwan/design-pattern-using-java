86b48a466c6caa06875403c59a4848283410206a
AmmanRizwan <ammanrizwan00007@gmail.com>
2026-04-05 16:16:15 +0530

    Adding an example of strategy design pattern using java

--- java/behavioral_pattern/strategy/Main.java
+++ java/behavioral_pattern/strategy/Main.java
@@ -2,6 +2,18 @@ package strategy;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Item item1 = new Item("Laptop", 12);
+        Item item2 = new Item("Phone", 11);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItem(item1);
+        cart.addItem(item2);
+
+        CreditCardPayment payment = new CreditCardPayment("1234-1234-2334", "Amman Rizwan");
+
+        cart.setPaymentStrategy(payment);
+
+        cart.checkout();
+
     }
 }
