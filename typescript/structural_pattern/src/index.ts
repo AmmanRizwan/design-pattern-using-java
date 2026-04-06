5c3127808d6e181227ef17bfdc1b88583e058ebe
AmmanRizwan <ammanrizwan00007@gmail.com>
2026-04-06 13:02:29 +0530

    Add one example of composite pattern in main

--- typescript/structural_pattern/src/index.ts
+++ typescript/structural_pattern/src/index.ts
@@ -7,6 +7,10 @@ import DVDPlayer from "./fascade/DVDPlayer";
 import Projector from "./fascade/Projector";
 import SoundSystem from "./fascade/SoundSystem";
 import Lights from "./fascade/Lights";
+import File from "./composite/File";
+import Folder from "./composite/Folder";
+import FileComponent from "./composite/FileComponent";
+
 
 // Adapter Example
 
@@ -63,4 +67,41 @@ threater.watchMovie("The White Night");
 console.log();
 threater.endMovie();
 
+// Composite Example
 
+console.log("\nComposite Example of File and Folder Class.\n");
+
+const file1: FileComponent = new File("document.txt", 100);
+const file2: FileComponent = new File("image.jpg", 500);
+const file3: FileComponent = new File("video.mp4", 2000);
+const file4: FileComponent = new File("music.mp3", 800);
+const file5: FileComponent = new File("index.ts", 20);
+const file6: FileComponent = new File("code.java", 34);
+
+const rootFolder: Folder = new Folder("root");
+const documentFolder: Folder = new Folder("document");
+const mediaFolder: Folder = new Folder("media");
+const projectFolder: Folder = new Folder("project");
+
+documentFolder.add(file1);
+
+mediaFolder.add(file2);
+mediaFolder.add(file3);
+mediaFolder.add(file4);
+
+projectFolder.add(file5);
+
+rootFolder.add(documentFolder);
+rootFolder.add(mediaFolder);
+rootFolder.add(projectFolder);
+
+console.log("\n=== File System Structure ===\n");
+rootFolder.showDetails(0);
+
+console.log("\n=== Total Size Calculation ===\n");
+console.log("Root Folder size: " + rootFolder.getSize() + " KB");
+console.log("Media Folder size: " + mediaFolder.getSize() + " KB");
+console.log("\n=== Removing Media Folder ===\n");
+rootFolder.remove(mediaFolder);
+rootFolder.showDetails(0);
+console.log("New root size: " + rootFolder.getSize() + " KB");
