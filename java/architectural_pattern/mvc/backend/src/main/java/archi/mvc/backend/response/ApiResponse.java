cecd360e7212d801db7f4d483eef6320d0d04878
AmmanRizwan <ammanrizwan00007@gmail.com>
2026-04-07 12:06:20 +0530

    response json for backend

---
+++ java/architectural_pattern/mvc/backend/src/main/java/archi/mvc/backend/response/ApiResponse.java
@@ -0,0 +1,15 @@
+package archi.mvc.backend.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ApiResponse<T> {
+    private String message;
+    private T data;
+}
