e2d4b65f99ebd225514bdbaa702dc54abe23b3b5
AmmanRizwan <ammanrizwan00007@gmail.com>
2026-04-09 13:19:37 +0530

    Example of the strategy design pattern

--- typescript/behavioral_pattern/src/index.ts
+++ typescript/behavioral_pattern/src/index.ts
@@ -2,6 +2,10 @@ import Tea from "./template_method/Tea";
 import Coffee from "./template_method/Coffee";
 import HotChocolate from "./template_method/HotChocolate";
 import Beverage from "./template_method/Beverage";
+import Item from "./strategy/Item";
+import ShoppingCart from "./strategy/ShoppingCart";
+import CreditCardPayment from "./strategy/CreditCardPayment";
+import PaymentStrategy from "./strategy/PaymentStrategy";
 
 // Template Method Example
 
@@ -18,3 +22,20 @@ console.log("\n=== Making Hot Chocolate ===\n");
 const hotChocolate: Beverage = new HotChocolate();
 hotChocolate._prepareBeverage();
 
+// Strategy Example
+
+console.log("\nExample of Strategy using the Shopping Cart Class.\n");
+
+const item1: Item = new Item("Laptop", 23);
+const item2: Item = new Item("Phone", 12);
+
+const cart: ShoppingCart = new ShoppingCart();
+cart.addItem(item1);
+cart.addItem(item2);
+
+const payment: PaymentStrategy = new CreditCardPayment("1234-1234-1234-1234", "Amman Rizwan");
+
+console.log("Total Amount: " + cart.calculateTotal());
+
+cart.setPaymentStrategy(payment);
+cart.checkOut();
