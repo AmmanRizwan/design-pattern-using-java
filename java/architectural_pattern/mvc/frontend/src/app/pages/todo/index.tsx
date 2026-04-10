7add5c7d11933f1d7ca78d266ad9e4b785804329
AmmanRizwan <ammanrizwan00007@gmail.com>
2026-04-10 13:35:08 +0530

    update the todos page using the tanstack react query

--- java/architectural_pattern/mvc/frontend/src/app/pages/todo/index.tsx
+++ java/architectural_pattern/mvc/frontend/src/app/pages/todo/index.tsx
@@ -1,54 +1,57 @@
-import { useEffect, useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
 import { createTodo, deleteTodoById, getTodos, setCompleteTodoById } from "../../../services/todo";
 import type { ITodo } from "../../../services/todo/interface";
 import { useNavigate } from "react-router-dom";
 
-interface IData {
-    task: string;
-}
-
 const Todos = () => {
+    const queryClient = useQueryClient();
+
+    const { data: todo, isLoading } = useQuery({
+        queryFn: () => getTodos(),
+        queryKey: ['todos'],
+    })
+
+    const { mutateAsync: submit } = useMutation({
+        mutationFn: (d: { task: string }) => createTodo(d),
+        onSuccess: () => {
+            queryClient.invalidateQueries(["todos"]);
+        },
+    });
+
+    const { mutateAsync: deleteById } = useMutation({
+        mutationFn: (id: string) => deleteTodoById(id),
+        onSuccess: () => {
+            queryClient.invalidateQueries(['todos']);
+        }
+    })
+
+    const { mutateAsync: updateComplete } = useMutation({
+        mutationFn: (id: string) => setCompleteTodoById(id),
+        onSuccess: () => {
+            queryClient.invalidateQueries(['todos']);
+        }
+    })
+
     const navigate = useNavigate();
-    const [data, setData] = useState<IData>({ task : "" });
-    const [todos, setTodos] = useState([]);
-
-    const submitData = async () => {
-        const response = await createTodo(data);
-        console.log(response.data);
-    }
-
-    const completedTodo = async (id: string) => {
-        const response = await setCompleteTodoById(id);
-        console.log(response);
-    }
-
-    const deleteData = async (id: string) => {
-        const response = await deleteTodoById(id);
-        console.log(response.data);
-    }
-
-    const fetchData = async () => {
-        const response = await getTodos();
-        setTodos(response.data);
-    }
+    const [task, setTask] = useState("");
     
-    useEffect(() => {
-        fetchData();
-    }, []);
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
 
     return (
         <div>
             <h1>Todo List</h1>
 
             <div>
-                <input type="text" value={data.task} onChange={(e) => setData({task: e.target.value})} /> <button onClick={() => submitData()}>Submit</button>
-                <button onClick={() => { console.log(todos) }}>Check the Data</button>
+                <input type="text" value={task} onChange={(e) => setTask(e.target.value)} /> <button onClick={async () => { await submit({task: task}); setTask("") }}>Submit</button>
             </div>
 
             <div>
                 <h3>Todo</h3>
                 {
-                    todos.map((value: ITodo, index) => {
+                    todo.data.map((value: ITodo, index: number) => {
                         return (
                             <div key={index} style={{ border: "1px"}}>
                                 <p>Task: {value.task}</p>
@@ -56,13 +59,13 @@ const Todos = () => {
                                     <p>Completed {value.is_completed ? "COMPLETED" : "NOT COMPLETED"}</p> 
                                     {
                                         !value.is_completed ? (
-                                            <input type="checkbox" onClick={async () => await completedTodo(value.id)} />
+                                            <input type="checkbox" onClick={async () => await updateComplete(value.id)} />
                                         ) : null
                                     }
                                 </div>
-                                <button onClick={() => { navigate(`/edit/${value.id}`)}}>Edit</button> <button  onClick={async () => {
-                                    await deleteData(value.id)
-                                }}>delete</button>
+                                <button onClick={() => {
+                                    navigate(`/edit/${value.id}`)
+                                }}>Edit</button> <button onClick={async () => { await deleteById(value.id)} }>delete</button>
                             </div>                            
                         )
                     })
