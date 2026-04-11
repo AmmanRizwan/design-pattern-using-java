83b568e3c5779e3f839c810eef36a47be2d2de0b
AmmanRizwan <ammanrizwan00007@gmail.com>
2026-04-11 13:33:23 +0530

    Implement all the state for the vending machine for process the data or state

---
+++ typescript/behavioral_pattern/src/state/SoldState.ts
@@ -0,0 +1,28 @@
+import { State } from "./State";
+import VendingMachine from "./VendingMachine";
+
+class SoldState implements State {
+    private machine: VendingMachine;
+
+    public constructor(machine: VendingMachine) {
+        this.machine = machine;
+    }
+
+    public insertCoin(): void {
+        
+    }
+
+    public ejectCoin(): void {
+        
+    }
+
+    public pressButton(): void {
+        
+    }
+
+    public dispense(): void {
+        
+    }
+}
+
+export default SoldState;
